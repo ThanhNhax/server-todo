8bb9a6e08ef5ee35462b8ab12080b16fd6efa1e7
thanhNha <thanhna.k13@gmail.com>
2023-10-06 00:07:39 +0700

    deploy server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const todoRouter = require('./src/routes/todoAppRoutes');
+const authRouter = require('./src/routes/authRoutes');
+// dung bien moi truong
+require('dotenv').config();
 const app = express();
-const port = 6060;
+const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/api/auth', authRouter);
+app.use('/api/todos', todoRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
